6b33c9e18c4b0604c6ec3cc39a62c112da2cd446
Ignacio Gómez <ignacio@schibsted.cl>
2018-04-15 11:14:43 -0300

    Delete products from package description

--- Package.swift
+++ Package.swift
@@ -5,13 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Vapor-Server",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Vapor-Server",
-            targets: ["Vapor-Server"]),
-        .executable(name: "Vapor-Server", targets: ["Vapor-Server"])
-    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
